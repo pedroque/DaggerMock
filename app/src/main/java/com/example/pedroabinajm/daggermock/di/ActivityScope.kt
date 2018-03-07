aebe51cea3b923c76ae5bd814e7a8e5730dbc624
pedroabinajm <pedroabinajm@gmail.com>
2018-03-07 18:54:33 -0300

    Added scopes

---
+++ app/src/main/java/com/example/pedroabinajm/daggermock/di/ActivityScope.kt
@@ -0,0 +1,8 @@
+package com.example.pedroabinajm.daggermock.di
+
+
+import javax.inject.Scope
+
+@Scope
+@Retention(AnnotationRetention.RUNTIME)
+annotation class ActivityScope
--- app/src/main/java/com/example/pedroabinajm/daggermock/view/hamburgers/HamburgersModule.kt
+++ app/src/main/java/com/example/pedroabinajm/daggermock/view/hamburgers/HamburgersModule.kt
@@ -1,5 +1,6 @@
 package com.example.pedroabinajm.daggermock.view.hamburgers
 
+import com.example.pedroabinajm.daggermock.di.ActivityScope
 import com.example.pedroabinajm.daggermock.mapper.HamburgerMapper
 import com.example.pedroabinajm.daggermock.viewmodel.ViewModelFactory
 import com.example.pedroabinajm.data.repository.HamburgerRepositoryImpl
@@ -7,15 +8,18 @@ import com.example.pedroabinajm.domain.interactor.GetHamburgers
 import com.example.pedroabinajm.domain.repository.HamburgerRepository
 import dagger.Module
 import dagger.Provides
+import dagger.Reusable
 
 @Module
 open class HamburgersModule {
 
     @Provides
+    @Reusable
     internal fun provideHamburgerRepository(hamburgerRepository: HamburgerRepositoryImpl)
             : HamburgerRepository = hamburgerRepository
 
     @Provides
+    @ActivityScope
     internal fun provideViewModelFactory(getHamburgers: GetHamburgers, hamburgerMapper: HamburgerMapper) =
             ViewModelFactory(getHamburgers, hamburgerMapper)
 
